9d894f0bcea98359cfc255dc0fcbccad337974d2
K D Hedger <kdhedger68713@gmail.com>
2024-07-30 16:35:23 +0100

    added some attributes manually

--- tagreader.cpp
+++ tagreader.cpp
@@ -1,22 +1,23 @@
 /*
  *
- * ©K. D. Hedger. Sat 27 Jul 15:57:43 BST 2024 keithdhedger@gmail.com
+ * ©K. D. Hedger. Tue 30 Jul 16:04:59 BST 2024 keithdhedger@gmail.com
 
- * This file (kkeditqttagreader.cpp) is part of KKEditQT.
+ * This file (tagreader.cpp) is part of MakeXLibDocset.
 
- * KKEditQT is free software: you can redistribute it and/or modify
+ * MakeXLibDocset is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
 
- * KKEditQT is distributed in the hope that it will be useful,
+ * MakeXLibDocset is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with KKEditQT.  If not, see <http://www.gnu.org/licenses/>.
+ * along with MakeXLibDocset.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 
 #include <QXmlStreamReader>
 #include <QFile>
